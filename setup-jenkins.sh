0def21ff427342737e4c80733dc3c219db93346b
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2021-07-02 13:38:14 +0100

    chore(deps): upgrade monitoring components
    * remove split of jobs / builds from Jenkins
     - it's nearly impossible to get the parsing / escape correct

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cat >> "${script_dir}/.env" <<END
-JAVA_OPTS='-Djava.util.logging.config.file=/var/jenkins_home/logging.properties -Djenkins.install.runSetupWizard=false -Djenkins.model.Jenkins.buildsDir=/build/\${ITEM_FULL_NAME} -Djenkins.model.Jenkins.workspacesDir=/workspace/\${ITEM_FULL_NAME}'
+JAVA_OPTS='-Djava.util.logging.config.file=/var/jenkins_home/logging.properties -Djenkins.install.runSetupWizard=false'
 JENKINS_OPTS="--httpPort=8080"
 JENKINS_SLAVE_AGENT_PORT="-1"
 DOCKER_GROUP_GID=$(getent group docker | awk -F: '{print $3}')
