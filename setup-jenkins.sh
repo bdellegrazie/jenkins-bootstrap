43e1b58e11af316ac5bad652427a8ab1380300bc
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2021-07-03 22:39:08 +0100

    fix(jenkins): allow separating builds and workspaces

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -5,9 +5,12 @@ IFS=$'\n\t'
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cat >> "${script_dir}/.env" <<END
-JAVA_OPTS='-Djava.util.logging.config.file=/var/jenkins_home/logging.properties -Djenkins.install.runSetupWizard=false'
+cat >> "${script_dir}/.env" <<'END'
+JAVA_OPTS='-Djava.util.logging.config.file=/var/jenkins_home/logging.properties -Djenkins.install.runSetupWizard=false  -Djenkins.model.Jenkins.buildsDir=/builds/\${ITEM_FULL_NAME} -Djenkins.model.Jenkins.workspacesDir=/workspaces/\${ITEM_FULL_NAME}'
 JENKINS_OPTS="--httpPort=8080"
 JENKINS_SLAVE_AGENT_PORT="-1"
+END
+
+cat >> "${script_dir}/.env" <<END
 DOCKER_GROUP_GID=$(getent group docker | awk -F: '{print $3}')
 END
